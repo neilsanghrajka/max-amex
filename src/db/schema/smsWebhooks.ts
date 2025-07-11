0deda1f20c1d23bfc091d1fbafeee929985fea9d
Neil Sanghrajka <neilsanghrajka@outlook.com>
2025-07-11 14:12:45 +0530

    Add prettier format

--- src/db/schema/smsWebhooks.ts
+++ src/db/schema/smsWebhooks.ts
@@ -3,15 +3,13 @@ import { sql } from "drizzle-orm";
 
 export const smsWebhooks = pgTable("SMS Webhooks", {
   // You can use { mode: "bigint" } if numbers are exceeding js number limitations
-  id: bigint({ mode: "number" })
-    .primaryKey()
-    .generatedByDefaultAsIdentity({
-      name: `"SMS Webhooks_id_seq"`,
-      startWith: 1,
-      increment: 1,
-      minValue: 1,
-      maxValue: 9223372036854775807,
-    }),
+  id: bigint({ mode: "number" }).primaryKey().generatedByDefaultAsIdentity({
+    name: `"SMS Webhooks_id_seq"`,
+    startWith: 1,
+    increment: 1,
+    minValue: 1,
+    maxValue: 9223372036854775807,
+  }),
   createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
     .defaultNow()
     .notNull(),
