ce6f1764919a946600ecda3e8b3a6eacc988c871
Neil Sanghrajka <neilsanghrajka@outlook.com>
2025-07-15 12:12:32 +0530

    Breakup payment function into steps

--- src/inngest/payment/gyftrLoginStep.ts
+++ src/inngest/payment/gyftrLoginStep.ts
@@ -1,3 +1,5 @@
 import { PaymentStep } from "@/inngest/payment/types";
 
-export const gyftrLoginStep = async (step: PaymentStep) => {console.log(step)};
+export const gyftrLoginStep = async (step: PaymentStep) => {
+  console.log(step);
+};
