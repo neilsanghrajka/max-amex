bc1f21452e1ba6735d517dfa423d81d336ca435d
Neil Sanghrajka <neilsanghrajka@outlook.com>
2025-07-15 12:12:19 +0530

    Breakup payment function into steps

---
+++ src/inngest/payment/gyftrLoginStep.ts
@@ -0,0 +1,3 @@
+import { PaymentStep } from "@/inngest/payment/types";
+
+export const gyftrLoginStep = async (step: PaymentStep) => {console.log(step)};
--- src/inngest/payment/types.ts
+++ src/inngest/payment/types.ts
@@ -1,7 +1,20 @@
 import { z } from "zod";
+import { type GetStepTools } from "inngest";
+import { inngest } from "@/inngest/client";
+import { EventNames } from "@/inngest/events";
 
 export const PaymentInitiateSchema = z.object({
   jobId: z.number(),
 });
 
 export type PaymentInitiateType = z.infer<typeof PaymentInitiateSchema>;
+
+export enum STEPS {
+  AMAZON_LOGIN = "amazon-login",
+  GYFTR_LOGIN = "gyft-login",
+}
+
+export type PaymentStep = GetStepTools<
+  typeof inngest,
+  typeof EventNames.PAYMENT_INITIATE
+>;
